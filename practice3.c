73a243d382ca02759e6eeb99aa7ae0fc979c1966
Bijoy-krishna <71882462+Bijoy-krishna@users.noreply.github.com>
2020-09-30 23:30:12 +0600

    Update practice3.c

--- practice3.c
+++ practice3.c
@@ -14,4 +14,21 @@ int main()
     return 0;
 }
 
+//Assignment 02
+#include <stdio.h>
+int main()
+{
+    int rows = 5, i, j, num = 1;
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf(" %d\t ", num);
+            num++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
 
